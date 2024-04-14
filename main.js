4873dfd450f90045cc0422ca250d097623107ada
Justar <136484541+Justar96@users.noreply.github.com>
2024-04-14 12:56:35 +0700

    0.5

--- main.js
+++ main.js
@@ -103,3 +103,12 @@ function doDate()
 }
 
 setInterval(doDate, 1000);
+
+$('body').terminal({
+  hello: function(what) {
+      this.echo('Hello, ' + what +
+                '. Wellcome to this terminal.');
+  }
+}, {
+  greetings: 'My First Web Terminal'
+});
